66cba7d38e1bf7b24db89433845a474696e0631c
Howard Kim <howardkim820@gmail.com>
2018-08-07 14:37:42 -0700

    completed

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -44,21 +44,7 @@ class ContactForm extends Component{
     }
     render(){
         const{firstName, lastName, phone, email} = this.state.form;
-
         return (
-            // <form onSubmit={this.handleSubmit}>
-            //     <div className="form-group">
-            //         <label>First Name </label>
-            //         <input onChange={this.handleInputChange} value={firstName} name="firstName" type="text"className="form-control"/>
-            //     </div>
-            //     <div className="form-group">
-            //         <label>Last Name </label>
-            //         <input onChange={this.handleInputChange} value={lastName} name="lastName" type="text" className="form-control" />
-            //     </div>
-            //     <button>Add Contact</button>
-            // </form>
-
-
             <form onSubmit={this.handleSubmit}>
                 <Field onChange={this.handleInputChange} name="firstName" label="First Name" type="text" value={firstName}/>
                 <Field onChange ={this.handleInputChange} name="lastName" label="Last Name" type="text" value = {lastName}/>
